d4bd6928bcd1c5398056ad5ca4ff9693918c9607
ReyPena <zonickblack@gmail.com>
2015-11-07 17:14:52 -0700

    Functional emailer

--- app/controllers/emailerController.js
+++ app/controllers/emailerController.js
@@ -1,3 +1,37 @@
 angular.module("mailApp").controller("emailerController",function ($scope, $http) {
+  $scope.email =[
+    {
+      emailAddress: "",
+      subject: "",
+      message: ""
+    }
+  ];
 
+  $scope.submit = function () {
+    var emailData = {
+      // TODO: Place key in file not tracked by git
+      key: "WxHumDCrhCP78sBDaNb9xg",
+      message: {
+        from_email: "mailapp@zonicblack.com",
+        to: [
+          {
+            email: $scope.email.emailAddress,
+            type: "to"
+          }
+        ],
+        subject: $scope.email.subject,
+        html: $scope.email.message
+      }
+    };
+
+    var request = {
+      method: "POST",
+      url: "https://mandrillapp.com/api/1.0/messages/send.json",
+      data: emailData
+    };
+    console.log(request);
+    $http(request).success(function successCallback(response){
+      console.log(response);
+    });
+  };
 });
